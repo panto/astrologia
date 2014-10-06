0f8984eef4c518fb101c534c21f98a5e595f18e4
Tomasz Stąpor <tomaszstapor@gmail.com>
2014-10-06 20:50:36 +0200

    Usunięcie zbędnego _site

--- _site/notes/baza-konfliktow-dplyr.R
+++
@@ -1,6 +0,0 @@
-library(dplyr)
-load("data/124920_1ucdpprioarmedconflictdataset4.2014.rdata")
-conflict_df <- tbl_df(ucdpConflict)
-conflict_df
-
-select(conflict_df, ConflictId,Location,Year,StartDate,StartPrec)
